76dcabe1d84049cc10ecdd02b6e37fd71ed2fbeb
Ashvin Bhat <ashvinbhat14@gmail.com>
2022-11-29 09:46:51 +0530

    removed-idProof

--- backend/models/vendorModel.js
+++ backend/models/vendorModel.js
@@ -10,7 +10,7 @@ const vendorSchema = new mongoose.Schema(
     address: { type: String, required: true },
     products: { type: [String], required: true },
     categories: {type: [String], required: true },
-    idproof: { type: String, required: true },
+    // idproof: { type: String, required: true },
   },
   {
     timestamps: true,
